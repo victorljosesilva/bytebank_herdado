20cb56a9cb4b253a708523a04d65d6f8dec35619
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-30 14:47:18 -0300

    cria atributo autenticador

--- src/bytebank_herdado/Gerente.java
+++ src/bytebank_herdado/Gerente.java
@@ -1,7 +1,8 @@
 package bytebank_herdado;
 
 public class Gerente extends Funcionario implements Autenticavel{
-
+	private Autenticador autenticador;
+	
 	@Override
 	public double getBonificacao() {
 		// TODO Auto-generated method stub
