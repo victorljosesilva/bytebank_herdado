f93f5b1b890156545f77f11da168bca6d6ca9154
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-30 14:13:19 -0300

    define Gerente como Autenticavel

--- src/bytebank_herdado/Gerente.java
+++ src/bytebank_herdado/Gerente.java
@@ -1,11 +1,23 @@
 package bytebank_herdado;
 
-public class Gerente extends Funcionario {
+public class Gerente extends Funcionario implements Autenticavel{
 
 	@Override
 	public double getBonificacao() {
 		// TODO Auto-generated method stub
 		return super.getSalario();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean autentica() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
