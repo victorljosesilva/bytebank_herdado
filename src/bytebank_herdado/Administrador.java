530dfa43f905a8bc1d5344b098b26853ba87fbc3
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-30 14:14:37 -0300

    define Administrador como Autenticavel

--- src/bytebank_herdado/Administrador.java
+++ src/bytebank_herdado/Administrador.java
@@ -1,11 +1,23 @@
 package bytebank_herdado;
 
-public class Administrador extends Funcionario {
+public class Administrador extends Funcionario implements Autenticavel {
 
 	@Override
 	public double getBonificacao() {
 		// TODO Auto-generated method stub
 		return 50;
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean autentica() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
