6a7161a1b92aca330a76059fe0cb3fc616f45e57
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-30 13:32:19 -0300

    cria class EditorDeVideo

---
+++ src/bytebank_herdado/EditorDeVideo.java
@@ -0,0 +1,11 @@
+package bytebank_herdado;
+
+public class EditorDeVideo extends Funcionario {
+
+	@Override
+	public double getBonificacao() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
