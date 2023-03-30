f0549180d8026a27e1984e3495444170a51a9a03
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-30 15:04:10 -0300

    cria method verificaAcesso

--- src/bytebank_herdado/AutenticacaoUtil.java
+++ src/bytebank_herdado/AutenticacaoUtil.java
@@ -2,4 +2,12 @@ package bytebank_herdado;
 
 public class AutenticacaoUtil {
 	private int senha = 222;
+	
+	public void verificaAcesso(Autenticavel a) {
+		if(a.autentica(senha)) {
+			System.out.println("Acesso permitido");
+		} else {
+			System.out.println("Acesso negado");
+		}
+	}
 }
