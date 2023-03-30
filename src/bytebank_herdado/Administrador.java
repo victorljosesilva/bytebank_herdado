c2530f9c1f676b808bdfdfc460e3f321b469e73d
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-30 13:31:08 -0300

    cria class Administrador

---
+++ src/bytebank_herdado/Administrador.java
@@ -0,0 +1,11 @@
+package bytebank_herdado;
+
+public class Administrador extends Funcionario {
+
+	@Override
+	public double getBonificacao() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
